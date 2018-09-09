38b56dcd39f33ba95a1e20188c81e7a530c2949d
ZHcoths <war3zhuhe@gmial.com>
2018-09-09 11:12:12 +0800

    start

---
+++ src/main/java/com/tianmaying/crawler/HtmlFetcher.java
@@ -0,0 +1,27 @@
+package com.tianmaying.crawler;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class HtmlFetcher {
+
+    public String fetch(String url) {
+        Connection.Response response = null;
+        try {
+            response = Jsoup.connect(url).timeout(3000).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
+        return null;
+    }
+
+    public static <T> void main(String[] args) throws Exception {
+        HtmlFetcher htmlFetcher = new HtmlFetcher();
+        System.out.println(htmlFetcher.fetch(
+                "http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0"));
+    }
+
+}
